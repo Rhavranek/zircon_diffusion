dfc0271491a7608ef8200d3aba0c9b4efa30f1ba
Rhavranek <rachel.havranek@colorado.edu>
2016-04-20 20:07:16 -0600

    model with 3 kinds of radiators
    need to mess with decay constants and etching ability

--- ca_landlab.py
+++ ca_landlab.py
@@ -6,21 +6,17 @@ Created on Mon Apr 11 15:44:15 2016
 #this is a code to simulate radiation damage and helium diffusion in zircon. 
 #The blue guys are heliums and the grey boxes are intact lattice pieces - I intend to 
 #make this code more complicated for my final project
-
 """
-
 """
 isotropic_turbulent_suspension.py
 Example of a continuous-time, stochastic, pair-based cellular automaton model, 
-which simulates the diffusion of suspended, neutrally buoyant particles in a
-turbulent fluid.
-
 
 """
 
 import time
 import matplotlib
-from numpy import where, arange
+import matplotlib.pyplot as plt
+from numpy import where, arange, bincount
 from landlab import RasterModelGrid
 from landlab.ca.celllab_cts import Transition, CAPlotter
 from landlab.ca.raster_cts import RasterCTS
@@ -67,13 +63,51 @@ def setup_transition_list():
     #    (left/bottom cell, right/top cell, orientation)
     #  - Transition rate (cells per time step, in this case 1 sec)
     #  - Name for transition
-    xn_list.append( Transition((0,2,0), (2,0,0), 1., 'left/down helium motion') )
-    xn_list.append( Transition((2,0,0), (0,2,0), 1., 'right/up helium motion') )
-    xn_list.append( Transition((1,0,0), (0,2,0), 0.01, 'decay') )
-    xn_list.append( Transition((0,1,0), (2,0,0), 0.01, 'decay') )
-    xn_list.append( Transition((1,1,0), (0,2,0), 0.01, 'decay') )
-    xn_list.append( Transition((1,1,0), (2,0,0), 0.01, 'decay') )
+    xn_list.append( Transition((0,2,0), (2,0,0), 2.0, 'left/down helium motion') )
+    xn_list.append( Transition((2,0,0), (0,2,0), 2.0, 'right/up helium motion') )
+    xn_list.append( Transition((1,0,0), (3,2,0), 0.000003, 'decayof238' )) #based on ppm in rock - 4 moleculs) )
+    xn_list.append( Transition((0,1,0), (2,3,0), 0.000003, 'decayof238') )
+    xn_list.append( Transition((1,0,0), (4,2,0), 0.000003, 'decay of Uleft238') )
+    xn_list.append( Transition((0,1,0), (2,4,0), 0.000003, 'decay of Uleft238') )
+    xn_list.append( Transition((3,0,0), (0,3,0), 0.0015, 'movement of u238 right/up'))#based on decay rate of u238) )
+    xn_list.append( Transition((3,1,0), (0,3,0), 0.0015, 'movement of u238 right/up'))
+    xn_list.append( Transition((0,4,0), (4,0,0), 0.0015, 'movement of u238 left/down') )
+    xn_list.append( Transition((1,4,0), (4,0,0), 0.0015, 'movement of u238 left/down') )
+    #xn_list.append( Transition((2,4,0), (4,2,0), 0.0015, 'let a nucleus swap with an alpha') )
+    #xn_list.append( Transition((4,2,0), (2,4,0), 0.0015, 'let a nucleus swap with an alpha') )
+    #xn_list.append( Transition((2,3,0), (3,2,0), 0.0015, 'let a nucleus swap with an alpha') )
+    #xn_list.append( Transition((3,2,0), (2,3,0), 0.0015, 'let a nucleus swap with an alpha') )
+    xn_list.append( Transition((3,0,0), (3,0,0), 0.0002, 'stopping of U238right'))#moves 8 times then stops) )
+    xn_list.append( Transition((0,3,0), (0,3,0), 0.0002, 'stopping of U238right') )
+    xn_list.append( Transition((4,0,0), (3,0,0), 0.0002, 'stopping of U238left') )
+    xn_list.append( Transition((0,4,0), (0,3,0), 0.0002, 'stopping of U238left') )
     
+    #xn_list.append( Transition((1,0,0), (5,2,0), 0.00016, 'decayof235') )
+    #xn_list.append( Transition((0,1,0), (2,5,0), 0.00016, 'decayof235') )
+    #xn_list.append( Transition((1,0,0), (6,2,0), 0.00016, 'decay of U235left') )
+    #xn_list.append( Transition((0,1,0), (2,6,0), 0.00016, 'decay of U235left') )
+    #xn_list.append( Transition((5,0,0), (0,5,0), 0.0098, 'movement of uranium right/up')) #based on decay rate u235) )
+    #xn_list.append( Transition((5,1,0), (0,5,0), 0.0098, 'movement of uranium right/up') )
+    #xn_list.append( Transition((0,6,0), (6,0,0), 0.0098, 'movement of uranium left/down') )
+    #xn_list.append( Transition((1,6,0), (6,0,0), 0.0098, 'movement of uranium left/down') )
+    #xn_list.append( Transition((5,0,0), (5,0,0), 0.0014, 'stopping of Uright'))#moves 7 times then stops) )
+    #xn_list.append( Transition((0,5,0), (0,5,0), 0.0014, 'stopping of Uright') )
+    #xn_list.append( Transition((6,0,0), (6,0,0), 0.0014, 'stopping of Uleft') )
+    #xn_list.append( Transition((0,6,0), (0,6,0), 0.0014, 'stopping of Uleft') )
+    
+    
+    xn_list.append( Transition((1,0,0), (7,2,0), 0.000001, 'decay') )
+    xn_list.append( Transition((0,1,0), (2,7,0), 0.000001, 'decay') )
+    xn_list.append( Transition((1,0,0), (8,2,0), 0.000001, 'decay of Uleft') )
+    xn_list.append( Transition((0,1,0), (2,8,0), 0.000001, 'decay of Uleft') )
+    xn_list.append( Transition((7,0,0), (0,7,0), 0.0004, 'movement of uranium right/up'))#based on decay rate of th) )
+    xn_list.append( Transition((7,1,0), (0,7,0), 0.0004, 'movement of uranium right/up') )
+    xn_list.append( Transition((0,8,0), (8,0,0), 0.0004, 'movement of uranium left/down') )
+    xn_list.append( Transition((1,8,0), (8,0,0), 0.0004, 'movement of uranium left/down') )
+    xn_list.append( Transition((7,0,0), (7,0,0), 0.00006, 'stopping of Uright'))#moves 6 times then stops) )
+    xn_list.append( Transition((0,7,0), (0,7,0), 0.00006, 'stopping of Uright') )
+    xn_list.append( Transition((8,0,0), (7,0,0), 0.00006, 'stopping of Uleft') )
+    xn_list.append( Transition((0,8,0), (0,7,0), 0.00006, 'stopping of Uleft') )
     return xn_list
     
     
@@ -84,8 +118,8 @@ def main():
     # User-defined parameters
     nr = 100  # number of rows in grid
     nc = 64  # number of columns in grid
-    plot_interval = 0.5   # time interval for plotting, sec
-    run_duration = 100.0   # duration of run, sec
+    plot_interval = 1.0   # time interval for plotting, sec -- each plot is 10^7 years
+    run_duration = 100.0   # duration of run, sec 
     report_interval = 10.0  # report interval, in real-time seconds
     
     # Remember the clock time, and calculate when we next want to report
@@ -102,7 +136,13 @@ def main():
     # Set up the states and pair transitions.
     ns_dict = { 0 : 'empty', 
                 1 : 'filled lattice',
-                2 : 'helium' }
+                2 : 'helium',
+                3 : 'uranium238right',
+                4 : 'uranium238left',
+                5 : 'uranium235right',
+                6 : 'uranium235left',
+                7 : 'thorium232right',
+                8 : 'thorium232left'}
     xn_list = setup_transition_list()
 
     # Create the node-state array and attach it to the grid
@@ -118,16 +158,24 @@ def main():
             node_state_grid[n] = 1 - node_state_grid[n]
     print(node_state_grid)
     
+    node_state_grid[0]=8
+    
     # For visual display purposes, set all boundary nodes to fluid
     node_state_grid[mg.closed_boundary_nodes] = 0
     
     # Create the CA model
     ca = RasterCTS(mg, ns_dict, xn_list, node_state_grid)
     
-    filled = '#696969'
-    empty = '#fffff0'
-    helium = '#00bfff'
-    clist = [empty, filled, helium]
+    filled = '#cdcdc1'
+    empty = '#ffffff'
+    helium = '#7fffd4'
+    uranium238right = '#ff1493'
+    uranium238left = '#ff1493'
+    uranium235right = '#8a2be2'
+    uranium235left = '#8a2be2'
+    thorium232right = '#f08080'
+    thorium232left = '#f08080'
+    clist = [empty, filled, helium, uranium238right, uranium238left, uranium235right, uranium235left, thorium232right, thorium232left]
     my_cmap = matplotlib.colors.ListedColormap(clist)
 
     # Create a CAPlotter object for handling screen display
@@ -155,11 +203,19 @@ def main():
         # Plot the current grid
         ca_plotter.update_plot()
         
+        print bincount(node_state_grid)
+        #plt.plot(bincount(node_state_grid))
+        #plt.ylabel('number of particles')
 
     # FINALIZE
 
     # Plot
+
     ca_plotter.finalize()
+    #plt.figure(2)
+    #plt.subplot(211)
+    #plt.show()
+    
 
 # If user runs this file, activate the main() function
 if __name__ == "__main__":
